27267e7beab24a1c864920cfc4091f97ff35d8c4
ShubhM28 <shubh.m2803@gmail.com>
2021-01-08 22:08:10 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,121 @@
+console.log("This is index.js");
+
+// Constructor
+function Book(name, author, type) {
+    this.name = name;
+    this.author = author;
+    this.type = type;
+}
+
+// Display Constructor
+
+function Display() {
+
+}
+
+//  Add methods to display prototype
+Display.prototype.add = function (book) {
+    console.log("Adding");
+    tableBody = document.getElementById('tableBody');
+    let uiString = `
+    <tr>
+    <td>${book.name}</td>
+    <td>${book.author}</td>
+    <td>${book.type}</td>
+  </tr>`;
+
+    tableBody.innerHTML += uiString;
+
+}
+
+// Implement the clear function
+Display.prototype.clear = function () {
+    let libraryForm = document.getElementById('libraryForm');
+    libraryForm.reset();
+}
+
+// Implement the validate function
+Display.prototype.validate = function (book) {
+    if (book.name.length < 2 || book.author.length < 2) {
+
+        return false;
+
+    }
+
+    else {
+        return true;
+    }
+}
+
+Display.prototype.show = function (type, displayMessage) {
+
+    let message = document.getElementById('message');
+    message.innerHTML = `
+    
+                <div class="alert alert-${type} alert-dismissible fade show" role="alert">
+                    <strong>Message :  </strong> ${displayMessage}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>`;
+
+    setTimeout(function () {
+        message.innerHTML = '';
+    }, 2000);
+
+
+
+
+}
+
+// Add submit event listener to libraryForm
+
+let libraryForm = document.getElementById('libraryForm');
+libraryForm.addEventListener('submit', libraryFormSubmit);
+
+function libraryFormSubmit(e) {
+    console.log('You have submitted the Library Form');
+    let name = document.getElementById('bookName').value;
+    let author = document.getElementById('author').value;
+    let type;
+
+    let fiction = document.getElementById('fiction');
+    let thriller = document.getElementById('thriller');
+    let biography = document.getElementById('biography');
+
+
+    if (fiction.checked) {
+        type = fiction.value;
+    }
+
+    else if (thriller.checked) {
+        type = thriller.value;
+    }
+
+    else if (biography.checked) {
+        type = biography.value;
+    }
+
+
+    let book = new Book(name, author, type);
+    console.log(book);
+
+    let display = new Display();
+    if (display.validate(book)) {
+
+        display.add(book);
+        display.clear();
+        display.show('Success', 'Your book has been added');
+    }
+
+    else {
+        // Show error to the user
+        display.show('Error', 'Sorry this book cannot be added.');
+    }
+
+
+
+
+    e.preventDefault();
+
+}
